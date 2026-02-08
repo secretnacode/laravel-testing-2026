7d9652187401ef0290ce12ad0614c891dde55e78
Clark David <clarkcatle8@gmail.com>
2026-02-08 17:46:54 +0800

    installation of laravel breeze

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,8 @@
 <?php
 
-use App\Http\Controllers\API\V1\testController as V1TestController;
-use App\Http\Controllers\API\V2\testController as V2TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
-
-Route::get('/test', function () {
-    return ["message" => "hellow"];
-});
-
-Route::prefix('v1')->group(function () {
-    Route::apiResource('posting', V1TestController::class);
 });
